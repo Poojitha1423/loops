ad13d0a1960a8c8afbcbcc5e03a43b9b747e9251
Poojitha <poojitha032320@gmail.com>
2022-08-17 20:00:42 +0530

    loops

---
+++ forloop.js
@@ -0,0 +1,4 @@
+var arr = [ 12,14,16,18,20];
+for { i = 0; i < 5;
+    i++;}
+console.log(arr);
---
+++ while loop.js
@@ -0,0 +1,12 @@
+var i =0;
+
+		do{
+
+			i++;
+
+		} while(i < 5)
+console.log(i);
+
+
+
+            
